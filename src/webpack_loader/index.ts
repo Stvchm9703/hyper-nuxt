16bce9ac282160ec795a6e0467fd1acf52cdc2f2
Steven Cheng <stv.chm@gmail.com>
2020-11-11 17:10:11 +0800

    update

---
+++ src/webpack_loader/index.ts
@@ -0,0 +1,38 @@
+// import type { Buffer } from "@types/node";
+
+import webpack from "webpack";
+import loaderUtils from "loader-utils";
+import { parse } from "querystring";
+import { RawSourceMap } from "source-map";
+import { generateCode, VueI18nLoaderOptions } from "./gen";
+
+const loader: webpack.loader.Loader = function (
+  source: string | Buffer,
+  sourceMap: RawSourceMap | undefined,
+): void {
+  const loaderContext = this; // eslint-disable-line @typescript-eslint/no-this-alias
+  const options = loaderUtils.getOptions(loaderContext) || {};
+
+  if (this.version && Number(this.version) >= 2) {
+    try {
+      // this.cacheable && this.cacheable();
+      const code = `${
+        generateCode(
+          source,
+          parse(this.resourceQuery),
+          options as VueI18nLoaderOptions,
+        )
+      }`;
+      this.callback(null, code, sourceMap);
+    } catch (err) {
+      this.emitError(err.message);
+      this.callback(err);
+    }
+  } else {
+    const message = "support webpack 2 later";
+    this.emitError(message);
+    this.callback(new Error(message));
+  }
+};
+
+export default loader;
